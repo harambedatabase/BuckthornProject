3d721bb9edd7ce2d336d484d75ab0f25782653d2
josiahtillman <josiahtillman@bethel-wifi.dhcp-207-11.vlan.net.its.bethel.edu>
2016-12-05 15:43:39 -0600

    Added PHP to add observation

---
+++ addObservationPHP.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns = "http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <title>Add Observation</title>
+  <meta http-equiv="Content-type" content="application/xhtml+xml; charset=utf-8">
+</head>
+<body>
+	<h2>Team Name: <?php print $_POST["teamName"]?></h2>
+</body>
+</html>
