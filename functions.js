25dccf95d7c5ee122803c5929aeb6e2ec6a69c0c
MichFont <michaelfontaine48@gmail.com>
2016-12-05 15:40:12 -0600

    Added add letter and add number functionality for biodiversity

--- functions.js
+++ functions.js
@@ -1,4 +1,14 @@
 function addMember(){
-    var newMember = document.getElementById("newMember").value;
-    document.getElementById("memberArea").value += newMember + "\n";
-  }
+  var newMember = document.getElementById("newMember").value;
+  document.getElementById("memberArea").value += newMember + "\n";
+};
+
+function addLetter(){
+  var newLetter = document.getElementById("newLetter").value;
+  document.getElementById("letterArea").value += newLetter + "\n";
+};
+
+function addNumber(){
+  var newNumber = document.getElementById("newNumber").value;
+  document.getElementById("numberArea").value += newNumber + "\n";
+};
