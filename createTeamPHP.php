20c367da92d44e0a147df532171230ce0f413b05
MichFont <michaelfontaine48@gmail.com>
2016-12-12 09:42:42 -0600

    Added the  "add member" capability
    Has not been tested yet, but should work based on everything I have seen

--- createTeamPHP.php
+++ createTeamPHP.php
@@ -2,18 +2,15 @@
 <body>
   <?php
         $con = mysqli_connect("localhost","mjf78594","A1G0r!tHm","ThisIsNotADatabase") or die("Some error occurred during connection " . mysqli_error($con));
-
-        $query = "INSERT INTO Student (Team_Name,Name) VALUES('".$_POST['teamName']."','".$_POST['name']."')";
-
-        $result = mysqli_query($con, $query);
-
-        if(!$result)
-        {
+        foreach($_SESSION['members'] as $name){
+          $query = "INSERT INTO Student (Team_Name,Name) VALUES('".$_POST['teamName']."','"$name"')";
+          $result = mysqli_query($con, $query);
+          if(!$result)
+          {
             die('Data could not be entered.' . mysql_error());
+          }
+          echo "Data entered successfully.\n";
         }
-
-        echo "Data entered successfully.\n";
-
         mysql_close($con);
   ?>
 
@@ -21,7 +18,6 @@
     <a href="index.html">Return to Index</a>
     </p>
     <p>
-    <a href="createTeam.html">Add Another Student</a>
     <p>
 </body>
 </html>
--- functions.js
+++ functions.js
@@ -1,9 +1,17 @@
 function addLetter(){
   var newLetter = document.getElementById("newLetter").value;
+  document.getElementById("newLetter").value = "";
   document.getElementById("letterArea").value += newLetter + "\n";
 };
 
 function addNumber(){
   var newNumber = document.getElementById("newNumber").value;
+  document.getElementById("newNumber").value = "";
   document.getElementById("numberArea").value += newNumber + "\n";
+};
+
+function addMember(){
+  var newMember = document.getElementById("memberName").value;
+  document.getElementById("memberName").value = "";
+  document.getElementById("memberArea").value += newMember + "\n";
 };
