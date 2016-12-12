f872f01f375d421d262e9a5cccabaa18a9155d74
josiahtillman <josiahtillman@bethel-wifi.dhcp-200-154.vlan.net.its.bethel.edu>
2016-12-11 20:47:46 -0600

    addObservation

--- addObservationPHP.php
+++ addObservationPHP.php
@@ -14,12 +14,17 @@
   $password = "A1G0r!tHm";
   $con = mysqli_connect("localhost",$username,$password,"ThisIsNotADatabase") or die("Some error occurred during connection " . mysqli_error($con));
 
-  //insert general
-  $query = "INSERT INTO General VALUES(" . $_POST["id"] . "," . $_POST["quadrantGPS_NS"] . "," . $_POST["quadrantGPS_EW"]
-                                         . "," . $_POST["quadrantSize"] . "," . $_POST["buckthornSize"] . "," . 
-                                         $_POST["density"] . "," . $_POST["buckthornCoverage"] . "," . $_POST["median"]
-                                         . "," . $_POST["habitat"] . "," . $_POST["photos"]. "," . $_POST["otherNotes"] . ");";
+  
 
+  //insert general
+  $query = "INSERT INTO General (QuadrantGPS_NS, QuadrantGPS_EW, QuadrantSize, BuckthornSize, Density, BuckthornCoverage, 
+                                 Median, Habitat, Photos, OtherNotes) VALUES(" . $_POST["quadrantGPS_NS"] . ", " 
+                                         . $_POST["quadrantGPS_EW"] . ", '" . $_POST["quadrantSize"] . "', " 
+                                         . $_POST["buckthornSize"] . ", '" . $_POST["density"] . "', " 
+                                         . $_POST["buckthornCoverage"] . ", '" . $_POST["median"] . "', '" 
+                                         . $_POST["habitat"] . "', '" . $_POST["photos"]. "', '" . $_POST["otherNotes"] . "'');";
+
+  echo $query . "\n";
   $result = mysqli_query($con, $query);
 
   if(!$result)
