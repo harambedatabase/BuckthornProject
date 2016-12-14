a8f0ab43dfdeba6df80d79a7e85dc9037fe35e56
ThirdEra <orsten.william@gmail.com>
2016-12-14 13:31:55 -0600

    switching teams to php

---
+++ teams.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+?>
+<html>
+<head>
+	<title>This Is Not A Database</title>
+  	<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+  	<script type="text/javascript">google.load("jquery", "1.3.2");</script>
+  	<script type="text/javascript" src="functions.js"></script>
+  	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="card">
+		<h2>Teams and Members</h2>
+
+
+
+
+
+
+
+
+
+
+		<a href="index.html">Back to Home</a>
+	</div>
+</body>
+</html>
