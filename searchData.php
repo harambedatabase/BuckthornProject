741fe7ebeb2d62a384f99a0288ae885da0b81fd3
ThirdEra <orsten.william@gmail.com>
2016-12-14 15:15:16 -0600

    search data by id is now done

--- searchData.php
+++ searchData.php
@@ -9,19 +9,28 @@
 	<div class="card">
 		<h2>Observations by Data ID</h2>
     <a href="generate.html">Back to Generate Reports</a>
+    <table border='1' id="teamTable">
+    <tr><th>Data ID</th><th>Team Name</th><th>Date</th></tr>
     <?php
         $username = "mjf78594";
         $password = "A1G0r!tHm";
      	  $con = mysqli_connect("localhost",$username,$password,"ThisIsNotADatabase") or die("Some error occurred during connection " . mysqli_error($con));
         $id = $_POST['data_id'];
-        echo $id;
-        $query = "SELECT * Team FROM Data where Data_ID='$id'";
+        $query = "SELECT * FROM Data where Data_ID='$id'";
     	  $result = mysqli_query($con, $query);
 
         if(!$result)
         {
             die('Data could not be entered.' . mysql_error());
         }
+
+        while($row = mysqli_fetch_array($result)) {	//mysqli_fetch_array grabs the next entry in the array
+	     	echo "<tr>
+	            <td>" , $row['Data_ID'] , "</td>
+	            <td>" , $row['Team_Name'], "</td>
+	            <td>" , $row['Date'], "</td>
+	          </tr>";
+	    	}
     ?>
 	</div>
 </body>
