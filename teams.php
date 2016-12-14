9c03e15de4f7bba344e1c3468f26da5c7d434d81
ThirdEra <orsten.william@gmail.com>
2016-12-14 15:01:30 -0600

    TEAMS.php now works
    Can now view report of teams and thier members

--- teams.php
+++ teams.php
@@ -1,12 +1,8 @@
-<?php
-  session_start();
-?>
 <html>
 <head>
-	<title>This Is Not A Database</title>
+    <title>This Is Not A Database</title>
   	<script src="http://www.google.com/jsapi" type="text/javascript"></script>
   	<script type="text/javascript">google.load("jquery", "1.3.2");</script>
-  	<script type="text/javascript" src="functions.js"></script>
   	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -17,7 +13,7 @@
         $username = "mjf78594";
         $password = "A1G0r!tHm";
      	  $con = mysqli_connect("localhost",$username,$password,"ThisIsNotADatabase") or die("Some error occurred during connection " . mysqli_error($con));
-        $query = "select * from Student";
+        $query = "SELECT DISTINCT Team FROM Student";
     	  $result = mysqli_query($con, $query);
 
         if(!$result)
@@ -28,14 +24,25 @@
     <table border='1' id="teamTable">
     <tr><th>Team Name</th><th>Members</th></tr>
     <?php
-    // iterate through the result set
-    while($row = mysqli_fetch_array($result)) {	//mysqli_fetch_array grabs the next entry in the array
+    while($row = mysqli_fetch_array($result)) {
      echo "<tr>
-            <td><form action='deleteConfirm.php' method='post'><input type='submit' value='Delete Observation' id='deleteObservation' style='width:100%'></td>
-            <td>" , $row['Data_ID'] , "</td>
-            <td>" , $row['Team_Name'], "</td>
-            <input type='hidden' value=",$row['Data_ID']," name='observationNumber'></form>
-          </tr>";
+            <td>" , $row['Team'] , "</td>";
+
+          $team = $row['Team'];
+          $queryNames = "SELECT Name FROM Student WHERE Team='$team'";
+      	  $resultTwo = mysqli_query($con, $queryNames);
+
+          if(!$resultTwo)
+          {
+              die('Data could not be entered.' . mysql_error());
+          }
+          echo "<td>";
+
+          while($rowTwo = mysqli_fetch_array($resultTwo)) {
+              echo $rowTwo['Name'] . nl2br("\n");
+          }
+          echo "</td>";
+          echo "</tr>";
     }
     ?>
 	</div>
