0e7fe4e8b0979252d03a6ae53e64372f386ae79e
MichFont <michaelfontaine48@gmail.com>
2016-12-14 15:20:39 -0600

    got the fields to populate

--- editConfirm.php
+++ editConfirm.php
@@ -7,54 +7,96 @@
    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+  <?php
+    $ObsNumber = $_POST['observationNumber'];
+    $username = "mjf78594";
+    $password = "A1G0r!tHm";
+    $con = mysqli_connect("localhost",$username,$password,"ThisIsNotADatabase") or die("Some error occurred during connection " . mysqli_error($con));
+    $query1 = "Select Team_Name from Data where Data_ID = $ObsNumber";
+    $teamName = mysqli_query($con, $query1);
+    $query2 = "select Date from Data where Data_ID = $ObsNumber";
+    $date = mysqli_query($con, $query2);
+    $query3 = "select QuadrantGPS_NS from General where Data_ID = $ObsNumber";
+    $gpsNS = mysqli_query($con, $query3);
+    $query4 = "select QuadrantGPS_EW from General where Data_ID = $ObsNumber";
+    $gpsEW = mysqli_query($con, $query4);
+    $query5 = "select QuadrantSize from General where Data_ID = $ObsNumber";
+    $quadSize = mysqli_query($con, $query5);
+    $query6 = "select BuckthornSize from General where Data_ID = $ObsNumber";
+    $numBuckthorn = mysqli_query($con, $query6);
+    $query7 = "select Density from General where Data_ID = $ObsNumber";
+    $density = mysqli_query($con, $query7);
+    $query8 = "select BuckthornCoverage from General where Data_ID = $ObsNumber";
+    $coverage = mysqli_query($con, $query8);
+    $query9 = "select Median from General where Data_ID = $ObsNumber";
+    $median = mysqli_query($con, $query9);
+    $query10 = "select Habitat from General where Data_ID = $ObsNumber";
+    $habitat = mysqli_query($con, $query10);
+    $query11 = "select photos from General where Data_ID = $ObsNumber";
+    $photos = mysqli_query($con, $query11);
+    $query12 = "select OtherNotes from General where Data_ID = $ObsNumber";
+    $notes = mysqli_query($con, $query12);
+    $query13 = "select BuckthornDBH from Competitive where Data_ID = $ObsNumber";
+    $dbh = mysqli_query($con, $query13);
+    $query14 = "select DistanceBN from Competitive where Data_ID = $ObsNumber";
+    $distanceBN = mysqli_query($con, $query14);
+    $query15 = "select BNDBH from Competitive where Data_ID = $ObsNumber";
+    $bndbh = mysqli_query($con, $query15);
+    $query16 = "select distanceNBN from Competitive where Data_ID = $ObsNumber";
+    $distanceNBN = mysqli_query($con, $query16);
+    $query17 = "select NBNDBH from Competitive where Data_ID = $ObsNumber";
+    $nbndbh = mysqli_query($con, $query17);
+    $query18 = "select competitionNotes from Competitive where Data_ID = $ObsNumber";
+    $compNotes = mysqli_query($con, $query18);
+  ?>
     <form action="addObservationPHP.php" method="post">
     <div class="card large">
         <h2>Team</h2>
         <br/>
         <h3>Enter your Team Name</h3>
-        <input type="text" name="teamName">
+        <input type="text" name="teamName" value=$teamName>
         <h3>Enter the Date(mm/dd/yy)</h3>
-        <input type="text" name="date">
+        <input type="text" name="date" value=$date>
     </div>
   	<div class="card large">
         <h2>General</h2>
         <br/>
         <h3>GPS Quadrant North/South:</h3>
-        <input type="text" name="quadrantGPS_NS">
+        <input type="text" name="quadrantGPS_NS" value=$gpsNS>
         <h3>GPS Quadrant East/West:</h3>
-        <input type="text" name="quadrantGPS_EW">
+        <input type="text" name="quadrantGPS_EW" value=$gpsEW>
         <h3>Quadrant Size:</h3>
-        <input type="text" name="quadrantSize">
+        <input type="text" name="quadrantSize" value=$quadSize>
         <h3>Number of Buckthorn Stems:</h3>
-        <input type="text" name="buckthornSize">
+        <input type="text" name="buckthornSize" value=$numBuckthorn>
         <h3>Density(# of stems/m^2):</h3>
-        <input type="text" name="density">
+        <input type="text" name="density" value=$density>
         <h3>% Buckthorn Foliar Coverage:</h3>
-        <input type="text" name="buckthornCoverage">
+        <input type="text" name="buckthornCoverage" value=$coverage>
         <h3>Median Buckthorn Stem Circumference:</h3>
-        <input type="text" name="median">
+        <input type="text" name="median" value=$median>
         <h3>Habitat Description:</h3>
-        <input type="text" name="habitat">
+        <input type="text" name="habitat" value=$habitat>
         <h3>Photos:</h3>
-        <input type="text" name="photos">
+        <input type="text" name="photos" value=$photos>
         <h3>Notes (opional):</h3>
-        <input type="text" name="otherNotes">
+        <input type="text" name="otherNotes" value=$notes>
     </div>
     <div class="card large">
         <h2>Competitive</h2>
         <br/>
         <h3>DBH of Buckthorn:</h3>
-        <input type="text" name="buckthornDBH">
+        <input type="text" name="buckthornDBH" value=$dbh>
         <h3>Distance to nearest buckthorn neighbor:</h3>
-        <input type="text" name="distanceBN">
+        <input type="text" name="distanceBN" value=$distanceBN>
         <h3>DBH of nearest buckthorn neighbor:</h3>
-        <input type="text" name="BNDBH">
+        <input type="text" name="BNDBH" value=$BNDBH>
         <h3>Distance to nearest non-buckthorn neighbor:</h3>
-        <input type="text" name="distanceNBN">
+        <input type="text" name="distanceNBN" value=$distanceNBN>
         <h3>DBH of nearest non-buckthorn neighbor:</h3>
-        <input type="text" name="NBNDBH">
+        <input type="text" name="NBNDBH" value=$nbndbh>
         <h3>Notes (opional):</h3>
-        <input type="text" name="competitionNotes">
+        <input type="text" name="competitionNotes" value=$compNotes>
     </div>
     <div class="card large">
         <h2>Biodiversity</h2>
