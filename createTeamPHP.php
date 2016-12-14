71ba14a5d0213fbcd8dea69bd0cf5f8786f35228
ThirdEra <orsten.william@gmail.com>
2016-12-13 18:43:36 -0600

    minor changes
    fixing some stuff

--- createTeamPHP.php
+++ createTeamPHP.php
@@ -1,47 +1,38 @@
 <html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
 <body>
-  <div class="card">
-  <?php
-    $con = mysqli_connect("localhost","mjf78594","A1G0r!tHm","ThisIsNotADatabase") or die("Some error occurred during connection " . mysqli_error($con));
+    <div class="card">
+    <?php
+        $con = mysqli_connect("localhost","mjf78594","A1G0r!tHm","ThisIsNotADatabase") or die("Some error occurred during connection " . mysqli_error($con));
 
-    $con->autocommit(false);
+        $con->autocommit(false);
+        $teamName = $_POST['teamName'];
+        $memberArea = trim($_POST['memberArea']);
+        $members = explode("\n",$memberArea);
 
+        foreach($members as $line){
 
-    $teamName = $_POST['teamName'];
-    $memberArea = trim($_POST['memberArea']);
+            $name = trim($line);
+            $query = "INSERT INTO Student VALUES(null,'" . $name . "', '" . $teamName . "')";
+            $result = $con->query($query);
 
-//  $members = explode("\n",$_POST['memberArea'],$members);
+            if(!$result)
+            {
+                die('Data could not be entered.' . mysql_error());
+            }
 
-    $members = explode("\n",$memberArea);
-
-    foreach($members as $line){
-
-        $name = trim($line);
-        $query = "INSERT INTO Student VALUES(null,'" . $name . "', '" . $teamName . "')";
-
-//echo $query,"\n";
-
-//      $result = mysqli_query($con, $query);
-        $result = $con->query($query);
-
-        if(!$result)
-        {
-             die('Data could not be entered.' . mysql_error());
+            mysqli_commit($con);
         }
 
-        echo "Data entered successfully.\n";
+        echo "Data entered successfully.","\n";
+        mysqli_close($con);
+        ?>
 
-        mysqli_commit($con);
-    }
-
-//  mysqli_commit($con);
-    mysqli_close($con);
-
-    ?>
-
-    <p>
-    <a href="index.html">Return to Index</a>
-    </p>
-  </div>
+        <p>
+            <a href="index.html">Return to Index</a>
+        </p>
+    </div>
 </body>
 </html>
